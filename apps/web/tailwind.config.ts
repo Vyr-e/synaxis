4d8dee176def7b1eac5791d10e24f1bf535aeaf3
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2025-02-04 17:52:11 +0100

    fix(web/tailwind.config.ts): Removed unnecessary configurations

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -2,19 +2,6 @@
 import { config } from '@repo/tailwind-config/config';
 import tailwindScrollbar from 'tailwind-scrollbar';
 
-config.content = [
-  './app/**/*.{js,ts,jsx,tsx,mdx}',
-  './components/**/*.{js,ts,jsx,tsx}',
-  '../../packages/design-system/**/*.{js,ts,jsx,tsx}', // Include shared components
-];
-
-// Safelist any classes that are dynamically created
-config.safelist = [
-  'bg-purple-100',
-  'text-purple-700',
-  // Add other dynamic classes here
-];
-
 config.plugins = [
   ...(config.plugins || []),
   tailwindScrollbar({ nocompatible: true }),
