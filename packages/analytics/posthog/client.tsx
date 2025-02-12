b52429524e4fcf58c5917844bd4cbca7aa9c4bbd
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2025-02-12 02:45:56 +0100

    fix(analytics): conditionally initialize PostHog for server-side rendering

--- packages/analytics/posthog/client.tsx
+++ packages/analytics/posthog/client.tsx
@@ -5,13 +5,17 @@ import posthogRaw, { type PostHog } from 'posthog-js';
 import { PostHogProvider as PostHogProviderRaw } from 'posthog-js/react';
 import type { ReactNode } from 'react';
 
-export const analytics = posthogRaw.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
-  api_host: '/ingest',
-  ui_host: env.NEXT_PUBLIC_POSTHOG_HOST,
-  person_profiles: 'identified_only',
-  capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-  capture_pageleave: true, // Overrides the `capture_pageview` setting
-}) as PostHog;
+const analytics =
+  typeof window !== 'undefined'
+    ? posthogRaw.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host: '/ingest',
+        ui_host: env.NEXT_PUBLIC_POSTHOG_HOST,
+
+        person_profiles: 'identified_only',
+        capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+        capture_pageleave: true, // Overrides the `capture_pageview` setting
+      })
+    : null;
 
 type PostHogProviderProps = {
   readonly children: ReactNode;
@@ -19,4 +23,6 @@ type PostHogProviderProps = {
 
 export const PostHogProvider = (
   properties: Omit<PostHogProviderProps, 'client'>
-) => <PostHogProviderRaw client={analytics} {...properties} />;
+) => <PostHogProviderRaw client={analytics as PostHog} {...properties} />;
+
+export { analytics };
