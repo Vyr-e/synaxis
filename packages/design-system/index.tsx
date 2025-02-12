25cc4128c10762b52a104af56eac85e561a4114c
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2025-02-12 02:26:10 +0100

    feat(design-system): enable AuthProvider and export utility functions

--- packages/design-system/index.tsx
+++ packages/design-system/index.tsx
@@ -1,11 +1,13 @@
 import { AnalyticsProvider } from '@repo/analytics';
-// import { AuthProvider } from '@repo/auth/provider';
+import { AuthProvider } from '@repo/auth/provider';
 import { env } from '@repo/env';
 import { VercelToolbar } from '@vercel/toolbar/next';
 import type { ThemeProviderProps } from 'next-themes';
 import { Toaster } from './components/ui/sonner';
 import { TooltipProvider } from './components/ui/tooltip';
 import { ThemeProvider } from './providers/theme';
+
+export { capitalize, cn } from './lib/utils';
 
 type DesignSystemProviderProperties = ThemeProviderProps;
 
@@ -14,14 +16,14 @@ export const DesignSystemProvider = ({
   ...properties
 }: DesignSystemProviderProperties) => (
   <ThemeProvider {...properties}>
-    {/* <AuthProvider> */}
-    <AnalyticsProvider>
-      <TooltipProvider>{children}</TooltipProvider>
-      <Toaster />
-      {env.NODE_ENV === 'development' && env.FLAGS_SECRET && env.TOOLBAR && (
-        <VercelToolbar />
-      )}
-    </AnalyticsProvider>
-    {/* </AuthProvider> */}
+    <AuthProvider>
+      <AnalyticsProvider>
+        <TooltipProvider>{children}</TooltipProvider>
+        <Toaster />
+        {env.NODE_ENV === 'development' && env.FLAGS_SECRET && env.TOOLBAR && (
+          <VercelToolbar />
+        )}
+      </AnalyticsProvider>
+    </AuthProvider>
   </ThemeProvider>
 );
