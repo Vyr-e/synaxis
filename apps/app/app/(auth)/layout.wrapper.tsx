5f3a26980c51c0da26a0d1179fee35f3529a8a99
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2025-02-13 00:51:00 +0100

    fix(theme): optimize auth layout theme switching logic

--- apps/app/app/(auth)/layout.wrapper.tsx
+++ apps/app/app/(auth)/layout.wrapper.tsx
@@ -7,10 +7,11 @@ import { useEffect } from 'react';
 
 export function AuthLayoutWrapper({ children }: { children: ReactNode }) {
   const pathname = usePathname();
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
 
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
-    setTheme(pathname.includes('sign') ? 'dark' : 'light');
-  }, [setTheme, pathname]);
+    setTheme(pathname.includes('auth') ? 'dark' : (theme as string));
+  }, []); // Let it run on mount this way it will be set to dark if the user is on the auth page and any other page will be set to the theme the user has set
   return <div className="bg-black">{children}</div>;
 }
