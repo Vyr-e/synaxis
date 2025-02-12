1499aef04340ae712b6c4670e47ff24cb252370e
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2025-02-12 02:46:49 +0100

    feat(tailwind): add color variables generation for dynamic theming

--- apps/app/tailwind.config.ts
+++ apps/app/tailwind.config.ts
@@ -1,1 +1,23 @@
-export { config as default } from '@repo/tailwind-config/config';
+import { config } from '@repo/tailwind-config/config';
+
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+config.plugins = [
+	...(config.plugins || []), 
+	addVariablesForColors
+] 
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
+
+export default config
