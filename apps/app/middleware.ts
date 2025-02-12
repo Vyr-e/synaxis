e5f2b617211f047ee4017c8fd5fd890d1fccbad1
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2025-02-12 02:47:55 +0100

    refactor(middleware): implement custom authentication middleware with session validation

--- apps/app/middleware.ts
+++ apps/app/middleware.ts
@@ -1,15 +1,49 @@
-import { authMiddleware } from '@repo/auth/middleware';
+import { PUBLIC_ROUTES } from '@repo/auth/public';
 import { noseconeConfig, noseconeMiddleware } from '@repo/security/middleware';
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
-const securityHeaders = noseconeMiddleware(noseconeConfig);
+const isPublicRoute = (request: NextRequest) => {
+  return PUBLIC_ROUTES.some((route) => request.url.includes(route));
+};
 
-export default authMiddleware(() => securityHeaders());
+//@ts-ignore
+export default async function middleware(request: NextRequest) {
+  const url = new URL('/api/auth/get-session', request.nextUrl.origin);
+
+  try {
+    await noseconeMiddleware(noseconeConfig)();
+
+    const response = await fetch(url, {
+      headers: {
+        cookie: request.headers.get('cookie') || '',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch session: ${response.statusText}`);
+    }
+    const session = await response.json();
+
+    if (!isPublicRoute(request) && !session) {
+      return NextResponse.redirect(new URL('/auth/sign-in', request.url));
+    }
+
+    return NextResponse.next();
+  } catch (error) {
+    // biome-ignore lint/suspicious/noConsole: Same as before this runs on edge best to leave it
+    console.error('Error in Middleware:', error);
+    return NextResponse.redirect(new URL('/error', request.url));
+  }
+}
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and all static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
+
+    '/((?!api|_next|ingest|.*\\..*).*)',
   ],
 };
