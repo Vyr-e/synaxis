79e5402e6bf1b9c8e55249c3e9150a993cb7202b
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2025-02-27 15:48:26 +0100

    refactor(ui): replace SVG icons with text buttons in cookie consent

--- apps/app/components/cookie-consent.tsx
+++ apps/app/components/cookie-consent.tsx
@@ -75,40 +75,14 @@ export function CookieConsent() {
                   onClick={() => handleConsent(false)}
                   className="rounded-md"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="size-5"
-                  >
-                    <title>Decline</title>
-                    <path
-                      fillRule="evenodd"
-                      d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                  Decline
                 </Button>
                 <Button
                   size="sm"
                   onClick={() => handleConsent(true)}
                   className="rounded-md"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="2"
-                    stroke="currentColor"
-                    className="size-5"
-                  >
-                    <title>Accept</title>
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m4.5 12.75 6 6 9-13.5"
-                    />
-                  </svg>
+                  Accept
                 </Button>
               </div>
             </div>
