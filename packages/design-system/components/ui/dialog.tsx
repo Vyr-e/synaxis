17fa1da8a4376d102b5738bcf82bb1bed8d83f8a
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2025-05-21 09:42:45 +0100

    fix(dialog): disable default close icon in DialogContent component
    - Updated the DialogContent component to set defaultCloseIcon to false, removing the close icon from the dialog by default.
    - This change simplifies the dialog's appearance and enhances customization options for users.

--- packages/design-system/components/ui/dialog.tsx
+++ packages/design-system/components/ui/dialog.tsx
@@ -37,7 +37,7 @@ interface DialogContentProps
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
 DialogContentProps
->(({ className, children, defaultCloseIcon = true, ...props }, ref) => (
+>(({ className, children, defaultCloseIcon = false, ...props }, ref) => (
   <DialogPortal>
     <DialogOverlay />
     <DialogPrimitive.Content
@@ -49,10 +49,6 @@ DialogContentProps
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-         {defaultCloseIcon && <Cross2Icon className="w-4 h-4" />}
-        <span className="sr-only">Close</span>
-      </DialogPrimitive.Close>
     </DialogPrimitive.Content>
   </DialogPortal>
 ))
