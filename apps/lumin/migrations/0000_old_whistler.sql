f4e401f887c167d4ad26412f29c6fc2e38c09589
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2025-07-15 00:48:58 +0100

    chore(apps/lumin): moved lumin migrations to lumin root for easy accessibility

---
+++ apps/lumin/migrations/0000_old_whistler.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS `user_profiles` (
+    `user_id` text PRIMARY KEY NOT NULL,
+    `created_at` text DEFAULT (CURRENT_TIMESTAMP),
+    `updated_at` text DEFAULT (CURRENT_TIMESTAMP),
+    `last_active_at` text DEFAULT (CURRENT_TIMESTAMP),
+    `demographics` json
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS `events` (
+    `id` text PRIMARY KEY NOT NULL,
+    `created_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+    `updated_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+    `metadata` json
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS `event_tags` (
+    `event_id` text NOT NULL,
+    `tag` text NOT NULL,
+    PRIMARY KEY (`event_id`, `tag`),
+    FOREIGN KEY (`event_id`) REFERENCES `events`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS `interactions` (
+    `id` text PRIMARY KEY NOT NULL,
+    `user_id` text NOT NULL,
+    `event_id` text NOT NULL,
+    `action` text NOT NULL,
+    `timestamp` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+    `metadata` json,
+    FOREIGN KEY (`user_id`) REFERENCES `user_profiles`(`user_id`) ON UPDATE no action ON DELETE cascade,
+    FOREIGN KEY (`event_id`) REFERENCES `events`(`id`) ON UPDATE no action ON DELETE cascade
+); 
